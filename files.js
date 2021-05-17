a73cdb854c0d1c02d5b8fcf7a18076fd886123b6
Le-TRex <clarembo@gmail.com>
2021-05-17 11:01:50 +0200

    :sparkles: add a condition to directory creation

--- files.js
+++ files.js
@@ -9,14 +9,21 @@ const fs = require("fs");
 }) */
 
 
-fs.writeFile("./docs/file1.txt", "Nouveau texte", () => {
+/* fs.writeFile("./docs/file1.txt", "Nouveau texte", () => {
     console.log("File written");
-});
+}); */
 
-fs.mkdir("./newDirectory", (err) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Dossier créé");
-    }
-})
+const directory = "./newDirectory"
+
+if(!fs.existsSync(directory)) {
+    fs.mkdir(directory, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Dossier créé");
+        }
+    })
+} else {
+    console.log("directory already exists")
+}
+
