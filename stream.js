c25622c6bba84207e9d742f24f83d4274786c053
Le-TRex <clarembo@gmail.com>
2021-05-17 11:48:07 +0200

    :sparkles: writeStream from readStream

--- stream.js
+++ stream.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream("./docs/file1.txt", {encoding: 'utf8'});
-
 /* fs.createReadStream(PATH, OBJET) ==> Iic l'objet sert à définir le type d'encodage */
+
+const writeStream = fs.createWriteStream("./docs/file2.txt");
 
 
 /* chunk = morceau de data */
 readStream.on('data', chunk => {
-    console.log(chunk);
+    writeStream.write(chunk, (err) => {
+        console.log("message écrit")
+    })
 });
-
-const writeStream = fs.createWriteStream("./docs/file2.txt");
-
-const chunk = "test"
-writeStream.write(chunk, (err) => {
-    console.log("message écrit")
-})
