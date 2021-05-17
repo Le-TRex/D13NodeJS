65f8c6229660ef4f4f73624a7a4f67ed2eccb1b7
Le-TRex <clarembo@gmail.com>
2021-05-17 15:41:59 +0200

    :recycle: refactor server.js

--- server.js
+++ server.js
@@ -1,46 +1,38 @@
 const http = require('http');
 const fs = require('fs');
 
-const home = "./views/home.html";
-const about = "./views/about.html";
-
 const server = http.createServer((request, response) => {
   console.log("request received");
   console.log(request.url, request.method);
   /* .setHeader = indiquer le type de réponse qu'on envoie au navigateur */
   response.setHeader("Content-Type", "text/html");
 
+  let path = "./views/";
+
   switch (request.url) {
     case "/" :
-      fs.readFile(home, (error, data)=> {
-        if(error){
-            console.log(error);
-        }else{
-          response.statusCode = 200;
-          response.write(data.toString());
-          response.end();
-        }
-      });
+      path += "home.html";
+      response.statusCode = 200;
       break;
     
     case "/about":
-      fs.readFile(about, (error, data)=> {
-        if(error){
-            console.log(error);
-        }else{
-          response.statusCode = 200;
-          response.write(data.toString());
-          response.end();
-        }
-      });
+        path += "about.html";
+        response.statusCode = 200;
       break;
 
     default:
+      path += "404.html";
       response.statusCode = 404;
-      response.write("<p>Not Found</p>");
-      response.end();
       break;
   }
+  fs.readFile(path, (error, data) => {
+    if(error){
+      console.log(error);
+    }else{
+      response.write(data.toString());
+    }
+    response.end();
+  })
 });
 
 server.listen(3000,'localhost', () => {
