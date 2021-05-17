39d0194f10eb3295b007cbb172c749cc5f6e6cc0
Le-TRex <clarembo@gmail.com>
2021-05-17 09:58:04 +0200

    :tada: setTimeout, setInterval, readFile, writeFile

---
+++ files.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// fs.readFile("./docs/file1.txt", (err, data)=> {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data.toString());
+//     }
+// })
+
+
+fs.writeFile("./docs/file1.txt", "Nouveau texte", () => {
+    console.log("File written");
+});
