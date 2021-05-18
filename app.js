e09b5cf561d2dc1d5af60fbcbc615fe10ea1e8c1
Le-TRex <clarembo@gmail.com>
2021-05-18 09:34:57 +0200

    :lipstick: add a middleware to use css file

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.listen(3002);
 
+app.use(express.static("public"));
+
+app.use((request, resopnse, next) => {
+  console.log(request.url, request.method);
+  next();
+})
+
 app.get("/", (request, response) => {
   response.sendFile("./views/home.html", { root: __dirname});
 })
