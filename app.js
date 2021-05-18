5e07b8b13de6faeeda5c30a9162b998648c4445f
Le-TRex <clarembo@gmail.com>
2021-05-18 09:08:34 +0200

    :truck: add routes /about and use for not found

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.listen(3002);
 app.get("/", (request, response) => {
   response.sendFile("./views/home.html", { root: __dirname});
 })
+
+app.get("/about", (request, response) => {
+  response.sendFile("./views/about.html", { root: __dirname});
+})
+
+/* Use doit être APRÈS les app.xxx */
+app.use((request, response) => {
+  response.status(404).sendFile("./views/404.html", { root: __dirname})
+});
